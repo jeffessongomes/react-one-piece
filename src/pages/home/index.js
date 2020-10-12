06a54ec0c3bab087ccb1cbfc3c6ea146ff675072
Jeffesson Gomes <jeffessong4@gmail.com>
2020-10-12 12:03:23 -0300

    :art: Criando conexao com a API

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import { Container } from './styles';
 import Card from '../../components/CardContainer';
+import { api } from '../../services/api';
 
 const Home = () => {
+  const [Chapters, setChapters] = useState([]);
+
+  useEffect(() => {
+    getChapters();
+  }, []);
+
+  const getChapters = useCallback(async () => {
+    const data = await api.get('');
+
+    console.log(data);
+  }, []);
+
   return (
     <Container>
       <ul>
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,3 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 0 400px;
+    list-style: none;
+
+    li {
+      padding: 13px;
+    }
+  }
+`;
